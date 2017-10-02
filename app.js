e8a8d69d76013028ee35423af68587a97f7999c7
kong <nopaket_kong@hotmail.com>
2017-10-02 21:19:35 +0700

    ok

---
+++ app.js
@@ -0,0 +1,10 @@
+//var ie  = require('./import-export')
+//import ie from './import-export';
+//console.log(ie)
+
+//import {x, y, z} from './import-export';
+//console.log(x,y,z)
+
+
+//import cube from './import-export';
+//console.log(cube(3));  // 27
---
+++ import-export.js
@@ -0,0 +1,20 @@
+//var myModule = { x: 1, y: function(){ return 'This is ES5'  }}
+//module.exports = myModule;
+
+
+
+
+//const myModule = { x: 1, y: () => { return 'This is ES6' }}
+//export default myModule;
+
+
+//export const x = 1;
+//export const y = 2;
+//export const z = 'String';
+
+
+
+//export default function cube(x) {
+//    return x * x * x;
+//}
+
