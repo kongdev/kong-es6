e28f242c58604acf4d64af713f5c912cf808c517
kongdev <nopaket_kong@hotmail.com>
2018-02-27 16:48:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 //var ie  = require('./import-export')
 //import ie from './import-export';
-//console.log(ie)
+//console.log(ie.x)
+//import ie  from './import-export';
+//console.log(ie.y())
 
-//import {x, y, z} from './import-export';
-//console.log(x,y,z)
+
+//import {x, y, z,kong} from './import-export';
+//console.log(x,y,z,kong())
 
 
 //import cube from './import-export';
 //console.log(cube(3));  // 27
+
+
+//var cube  = require('./import-export')
+//console.log(cube(3))
