e5d7d896d5ab2872fb5042405f8e76337f2102c6
kongdev <nopaket_kong@hotmail.com>
2018-02-27 14:23:36 +0700

    Update class.js

--- class.js
+++ class.js
@@ -1,1 +1,32 @@
-console.log('zzz')
+import Base from './base'
+
+class Service extends Base {
+  
+  constructor() {
+    super()
+    this.foo = 15
+    this.test = this.test.bind(this)
+    this.methodA = this.methodA.bind(this);
+    
+  }
+  
+  test(a, b) {
+        return a + b
+  }
+  
+  methodA(){
+    
+  }
+  
+  methodB(){
+  
+  }
+  
+  methodC(){
+    console.log(this.test(1, 2))
+  }
+  
+}
+
+
+export default Service
